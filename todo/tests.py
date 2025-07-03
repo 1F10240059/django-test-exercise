62a9aac31b93a9f20a6d19f5420b056c39aca53d
1F10240059 <s1f102400594@iniad.org>
2025-07-03 10:58:35 +0900

    requirements.txtの追加

--- todo/tests.py
+++ todo/tests.py
@@ -80,8 +80,8 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(response.context['tasks'][0], task2)
-        self.assertEqual(response.context['tasks'][1], task1)
+        self.assertEqual(response.context['tasks'][0], task1)
+        self.assertEqual(response.context['tasks'][1], task2)
 
     
     def test_index_get_order_due(self):
