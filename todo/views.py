64a07304203473e2fb53b16563cad99f66f43338
1F10240059 <s1f102400594@iniad.org>
2025-07-03 11:40:15 +0900

    警告文の修正

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,8 @@ from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
 # Create your views here.
+
+
 def index(request):
     if request.method == 'POST':
         task = Task(title=request.POST['title'],
@@ -16,7 +18,7 @@ def index(request):
         tasks = Task.objects.order_by('-posted_at')
 
     tasks = Task.objects.all()
-    
+
     context = {
         'tasks': tasks
     }
